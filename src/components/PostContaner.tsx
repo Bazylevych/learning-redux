550677acbe01061275974c0a759e4dafa8981fb6
thew9rst <79503555+Bazylevych@users.noreply.github.com>
2023-05-16 18:54:11 +0300

    Over

--- src/components/PostContaner.tsx
+++ src/components/PostContaner.tsx
@@ -1,10 +1,14 @@
 import { useEffect, useState } from "react";
 import { postAPI } from "../services/PostService";
 import PostItem from "./PostItem";
+import { IPost } from "../models/IPost";
 
 export default function PostContainer() {
-  const [limit, setLimit] = useState<number>(10);
-
+  const [limit, setLimit] = useState<number>(100);
+  const [createPost, { error: createError, isLoading: isCreateLoading }] =
+    postAPI.useCreatePostMutation();
+  const [deletePost] = postAPI.useDeletePostMutation();
+  const [updatePost] = postAPI.useUpdatePostMutation();
   const {
     data: posts,
     error,
@@ -15,18 +19,40 @@ export default function PostContainer() {
   });
 
   useEffect(() => {
-    setTimeout(() => {
-      setLimit(5);
-    }, 5000);
+    // setTimeout(() => {
+    //   setLimit(5);
+    // }, 5000);
   }, []);
+
+  const handleCreate = async () => {
+    const title = prompt();
+    await createPost({ title, body: title } as IPost);
+  };
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post);
+  };
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post);
+  };
 
   return (
     <div>
       <div className="post-list">
+        <button onClick={handleCreate}>Add Post</button>
         {/* <button onClick={() => refetch()}>REFETCH</button> */}
         {isLoading && <h1>Loading...</h1>}
         {error && <h1>Error</h1>}
-        {posts && posts.map((post) => <PostItem key={post.id} post={post} />)}
+        {posts &&
+          posts.map((post) => (
+            <PostItem
+              key={post.id}
+              post={post}
+              remove={handleRemove}
+              update={handleUpdate}
+            />
+          ))}
       </div>
     </div>
   );
--- src/components/PostContanerCopy.tsx
+++ src/components/PostContanerCopy.tsx
@@ -1,14 +1,33 @@
+import { IPost } from "../models/IPost";
 import { postAPI } from "../services/PostService";
 import PostItem from "./PostItem";
 
 export default function PostContainerCopy() {
-  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(10);
+  const { data: posts, error, isLoading } = postAPI.useFetchAllPostsQuery(100);
+  const [deletePost] = postAPI.useDeletePostMutation();
+  const [updatePost] = postAPI.useUpdatePostMutation();
+
+  const handleRemove = (post: IPost) => {
+    deletePost(post);
+  };
+
+  const handleUpdate = (post: IPost) => {
+    updatePost(post);
+  };
   return (
     <div>
       <div className="post-list">
         {isLoading && <h1>Loading...</h1>}
         {error && <h1>Error</h1>}
-        {posts && posts.map((post) => <PostItem key={post.id} post={post} />)}
+        {posts &&
+          posts.map((post) => (
+            <PostItem
+              key={post.id}
+              post={post}
+              remove={handleRemove}
+              update={handleUpdate}
+            />
+          ))}
       </div>
     </div>
   );
